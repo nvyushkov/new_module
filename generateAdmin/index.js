19e6b5667fcfe97b0aa339e75cea86daa7038c52
Nickita Vyushkov <wimanen@ya.ru>
2016-04-08 19:25:13 +0300

    Добавил комментарии к коду

--- generateAdmin/index.js
+++ generateAdmin/index.js
@@ -4,38 +4,42 @@ var Widget      = require('adminDBModels/widgets').Widget;
 var async       = require('async');
 var Models      = [ Menu, Material, Widget]; // добавляем наши модеи в массив
 
+// загружаем модульные части
 var materialParse   = require('./jsonDataPage/materials');
 var menuParse       = require('./jsonDataPage/menus');
 var widgetParse     = require('./jsonDataPage/widgets');
 
+// функция забирает данные из базы
 var adminElementsRead = function() {
-    Models.forEach(function (model) {  // для каждой коллекции
+    Models.forEach(function (model) {         // для каждой коллекции в массиве Models
         model.find({}, function (err, data) { // ищем в текущей коллекции все данные
             if (err) throw err;
-            console.log(data)
+            console.log(data)                 // выводим полученные данные
         });
     });
 };
 
+// запускаем асинхронную функцию
 async.waterfall([
     function(callback) {
-        materialParse( function(err, result) {
+        materialParse( function(err, result) {    // вызываем ункцию модуля Материалов, получаем результат и отдаем дальше
             if (err) { callback("Материалы jSON не считаны") }
             callback(null, result)
         })
     },
     function(result, callback) {
-        menuParse( function(err, result) {
+        menuParse( function(err, result) {    // вызываем ункцию модуля Меню, получаем результат и отдаем дальше
             if (err) { callback("Меню jSON не считаны") }
             callback(null, result)
         })
     },
     function(result, callback) {
-        widgetParse( function(err, result) {
+        widgetParse( function(err, result) {    // вызываем ункцию модуля Виджетов, получаем результат и отдаем дальше
             if (err) { callback("Виджеты jSON не считаны") }
             callback(null, result)
         })
     }
 ], function(err, result){
+    // Если результат модулей лементов вернулись без ошибок - запускаем функцию чтения из базы
     result == 1 ? adminElementsRead() : console.log("error")
 });
--- generateAdmin/jsonDataPage/materials/index.js
+++ generateAdmin/jsonDataPage/materials/index.js
@@ -1,22 +1,23 @@
 var fs          = require('fs');
-var Model       = require('adminDBModels/materials').Material;
+var Model       = require('adminDBModels/materials').Material;// загружаем можель модуля
 var path        = require('path');
-var yum 	    = 0;                                        // счетчик для определения завершения чтения
-var directory   = 'jsonDataPage/materials';
-var jsonFiles   = [];
+var yum         = 0;                                      // счетчик для определения завершения чтения
+var directory   = 'jsonDataPage/materials';               // путь к нашему модулю, исходя из родительского
+var jsonFiles   = [];                                     // промежуточный массив json файлов
 
 module.exports = function (callback){
-    fs.readdir( directory, function(err, files){ 	        // открываем текущую директорию
+    fs.readdir( directory, function(err, files){ 	        // открываем директорию
         if (err) { console.log(err) }
-        files.forEach( function(filename){
+        files.forEach( function(filename){                // проверяем все файлы директории, файлы jSON добавляем в массив jsonFile
             path.extname(filename) == ".json" ? jsonFiles.push(filename) : false;
         });
-        jsonFiles.forEach( function(jsonFile){		            // для каждого файла в текущей директории
+        jsonFiles.forEach( function(jsonFile){		        // перебираем все jSON файлы в массиве jsonFile
             fs.readFile(directory+'/'+jsonFile, function(err, data) { // читаем json файл
-                var saveD = new Model(JSON.parse(data));// данные из файла json в переменную
+                var saveD = new Model(JSON.parse(data));// данные из файла json в переменную, парся в формате jSON
                 saveD.save(function(err) {              // сохраняем наши данные
                     if (err) throw err;
-                    yum+=1;
+                    yum+=1; // считаем записанные файлы
+                    // если количество записанных файлов равна их количеству вмассиве jsonFiles, запускаем колбек
                     yum==jsonFiles.length ? callback(null, 1) : false
                 });
             });
