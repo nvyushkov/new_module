0169e630ce2727e835039a924c45d2e24c1ea95b
Nickita Vyushkov <wimanen@ya.ru>
2016-04-08 19:12:43 +0300

    Препродакшн - разбил по модулям

--- generateAdmin/index.js
+++ generateAdmin/index.js
@@ -1,42 +1,41 @@
-var fs          = require('fs');
 var Menu        = require('adminDBModels/menus').Menu;
 var Material    = require('adminDBModels/materials').Material;
 var Widget      = require('adminDBModels/widgets').Widget;
-var path        = require('path');
-var dir_d       = ['jsonDataPage/menus','jsonDataPage/materials','jsonDataPage/widgets']; // все пути для jSON файлов внутри модуля
-var param_S     = [ Menu, Material, Widget]; // добавляем наши модеи в массив
-var yum 	= 0; // счетчик для определения завершения чтения
+var async       = require('async');
+var Models      = [ Menu, Material, Widget]; // добавляем наши модеи в массив
 
-function pushDataJson(){
-    dir_d.forEach( function(directory){  		// для каждого элемента массива директорий
-        fs.readdir( directory, function(err, files){ 	// открываем текущую директорию
-            if (err) { console.log(err)}		
-            files.forEach( function(filename){		// для каждого файла в текущей директории
-                if ( path.extname(filename) == ".json" ) { // проверяем расширение файла на .json
-                    fs.readFile(directory+'/'+filename, function(err, data) { // читаем json файл
-                        var indexDir = dir_d.indexOf(directory); // берем индекс директории в массиве
-                        var model = param_S[indexDir]; // считаем нужную нам модель в массиве моделе
-                        var saveD = new model(JSON.parse(data)); // данные из файла json в переменную
-                        saveD.save(function(err) { // сохраняем наши данные
-                            if (err) throw err;
-                            yum+=1; // увеличиваем количество пройденных элементов
-				// если прошли все элементы и записали в базу - вызываем функцию чтения
-                            yum==dir_d.length? read() : false 
-                        });
-                    });
-                }
-            } );
-        });
-    });
-}
+var materialParse   = require('./jsonDataPage/materials');
+var menuParse       = require('./jsonDataPage/menus');
+var widgetParse     = require('./jsonDataPage/widgets');
 
-function read(){
-    param_S.forEach(function (modelName) {  // для каждой коллекции
-        modelName.find({}, function (err, data) { // ищем в текущей коллекции все данные
+var adminElementsRead = function() {
+    Models.forEach(function (model) {  // для каждой коллекции
+        model.find({}, function (err, data) { // ищем в текущей коллекции все данные
             if (err) throw err;
-            console.log(data); // выводим данные из базы
+            console.log(data)
         });
     });
-}
+};
 
-pushDataJson();  //вызываем функцию работы с файлами json
+async.waterfall([
+    function(callback) {
+        materialParse( function(err, result) {
+            if (err) { callback("Материалы jSON не считаны") }
+            callback(null, result)
+        })
+    },
+    function(result, callback) {
+        menuParse( function(err, result) {
+            if (err) { callback("Меню jSON не считаны") }
+            callback(null, result)
+        })
+    },
+    function(result, callback) {
+        widgetParse( function(err, result) {
+            if (err) { callback("Виджеты jSON не считаны") }
+            callback(null, result)
+        })
+    }
+], function(err, result){
+    result == 1 ? adminElementsRead() : console.log("error")
+});
---
+++ generateAdmin/jsonDataPage/materials/index.js
@@ -0,0 +1,25 @@
+var fs          = require('fs');
+var Model       = require('adminDBModels/materials').Material;
+var path        = require('path');
+var yum 	    = 0;                                        // счетчик для определения завершения чтения
+var directory   = 'jsonDataPage/materials';
+var jsonFiles   = [];
+
+module.exports = function (callback){
+    fs.readdir( directory, function(err, files){ 	        // открываем текущую директорию
+        if (err) { console.log(err) }
+        files.forEach( function(filename){
+            path.extname(filename) == ".json" ? jsonFiles.push(filename) : false;
+        });
+        jsonFiles.forEach( function(jsonFile){		            // для каждого файла в текущей директории
+            fs.readFile(directory+'/'+jsonFile, function(err, data) { // читаем json файл
+                var saveD = new Model(JSON.parse(data));// данные из файла json в переменную
+                saveD.save(function(err) {              // сохраняем наши данные
+                    if (err) throw err;
+                    yum+=1;
+                    yum==jsonFiles.length ? callback(null, 1) : false
+                });
+            });
+        });
+    });
+};
