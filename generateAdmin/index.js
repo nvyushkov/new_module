bd8a6b8a6ea2412bad132f774904033f6c94c104
Nikita Vyushkov <wimanen@ya.ru>
2016-04-11 10:02:00 +0300

    правки синтаксиса - ;

--- generateAdmin/index.js
+++ generateAdmin/index.js
@@ -4,9 +4,9 @@ var jsonDataPage    = require('./jsonDataPage');
 // иоздаем функцию, которая проверяет что вернул модуль парсинга jsonDataPage
 var generateAdmin   = function(callback) {
     jsonDataPage( function(err, result) {    // проверяем что нам вернул модуль и вызываемколбэк
-        if (err) { callback("файлы jSON не считаны") }
-        callback(null, result)
-    })
+        if (err) { callback("файлы jSON не считаны") };
+        callback(null, result);
+    });
 };
 
 generateAdmin(function(err, result) { console.log(result)});
--- generateAdmin/jsonDataPage/index.js
+++ generateAdmin/jsonDataPage/index.js
@@ -17,25 +17,25 @@ var saveD       = '';
 module.exports = function(callback) {
     dirArr.forEach(function( directory ){                       // перебираем директории
         fs.readdir( directory, function(err, files){ 	        // открываем директорию
-            if (err) { console.log(err) }
+            if (err) { console.log(err) };
             files.forEach( function(jsonFile){		        // перебираем все jSON файлы в массиве jsonFile
                 if (path.extname(jsonFile) == ".json") {
                     fs.readFile(directory+'/'+jsonFile, function(err, data) { // читаем json файл
-                        if (err) { console.log(err)}
+                        if (err) { console.log(err)};
                         index = dirArr.indexOf(directory);
                         model = modArr[index];
                         saveD = new model(JSON.parse(data));    // данные из файла json в переменную, парся в формате jSON
                         saveD.save(function(err) {                  // сохраняем наши данные
-                            if (err) { console.log(err) }
+                            if (err) { console.log(err) };
                         });
                     });
-                }
+                };
             });
             yum+=1; // считаем записанные файлы
             // если количество записанных файлов равна их количеству вмассиве jsonFiles, запускаем колбек
             if ( yum==dirArr.length ){
-                callback(null, "Все jsonFiles записаны")
-            }
+                callback(null, "Все jsonFiles записаны");
+            };
         });
     });
 };
